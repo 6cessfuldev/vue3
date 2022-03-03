d92fd030324f7730d679d79414c0f9257f5c6b0f
육성민 <6cessfuldev@gmail.com>
2022-03-03 14:27:48 +0000

    second lesson

--- vue3_project/src/App.vue
+++ vue3_project/src/App.vue
@@ -1,3 +1,40 @@
 <template>
- <div>Kossie coder</div>
+	<div>
+	<div class="name">{{ name }}</div>
+	<input type="text" v-bind:value="name"> 
+	<button 
+		class="btn btn-primary"
+		v-on:click="consoleLog">
+		Click
+	</button>
+	</div>
 </template>
+
+<script>
+import { ref } from 'vue' ;
+	
+export default {
+	setup() {
+		const name = ref('Kossie Coder');
+		
+		const myName = "Yuk"
+		
+		const greeting = (name) => {
+			return 'Hello, ' + name;
+		}
+		
+		const consoleLog = () => {
+			name.value = name.value + '1';
+		};
+		
+		return {
+			name,
+			myName,
+			greeting,
+			consoleLog,
+		};
+	}
+}
+</script>
+
+<style></style>
