5d25b68c3b58a675a99b1b839c52f54ea905b79b
육성민 <6cessfuldev@gmail.com>
2022-03-05 14:38:13 +0000

    부모 컴포넌트와 자식 컴포넌트 간의 데이터와 이벤트 전달

--- vue3_project/src/App.vue
+++ vue3_project/src/App.vue
@@ -1,80 +1,54 @@
 <template>
 	<div class="container">
 		<h2>To-Do List</h2>
-		<form v-on:submit.prevent="addList">
-			<div class="d-flex">
-				<div class="flex-frow-1 mr-2">
-					<input class="form-control" type="text" v-model="todo" placeholder="Type new to-do"> 
-				</div>
-				<div>
-					<button class="btn btn-primary" type="submit">
-						Add
-					</button>	
-				</div>
-			</div>
-			
-			<div v-show="hasError" style="color: red">
-				This field cannnot be empty
-			</div>
-			
-		</form>
-		<div class="card mt-2" v-for="todo in todos" :key="todo.id">
-			<div class="card-body p-2 d-flex align-items-center">
-				<div class="form-check flex-grow-1">
-					<input class="form-check-input" type="checkbox" v-model="todo.completed">
-					<label class="form-check-label" :class="{todo: todo.completed}">
-						{{ todo.subject }}
-					</label>
-				</div>
-				<div>
-					<button class="btn btn-danger tn-sm" @click="deleteTodo">
-						Delete
-					</button>
-				</div>
-			</div>		
+		<TodoSimpleForm @add-todo="addTodo" />
+		
+		<div v-if="!todos.length">
+			추가된 Todo가 없습니다.
 		</div>
+		
+		<TodoList :todos="todos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo"/>
+		
 	</div>
 </template>
 
 <script>
 import { ref } from 'vue' ;
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
+import TodoList from './components/TodoList.vue';
 	
 export default {
+	components: {
+		TodoSimpleForm,
+		TodoList,
+	},
 	setup() {
-		const todo = ref('');
-		const todos = ref([]);		
 		
-		const hasError = ref(false);
+		const todos = ref([]);		
 		
 		const todoStyle = {
 			textDecoration: 'line-through',
 			color: 'gray'
 		}
 		
-		
-		const addList = () => {
-			if(todo.value === ''){
-				hasError.value = true;
-			} else {
-				hasError.value = false;
-				todos.value.push({
-				id: Date.now(),
-				subject: todo.value,
-				completed: false,
-			})
-			todo.value="";} 
+		const addTodo = (todo) => {
+			console.log(todo);
+				todos.value.push(todo);
 			
 		};
 		
-		const deleteTodo = () => {
-			console.log('delete todo')
-		}
+		const toggleTodo = (index) => {
+			todos.value[index].completed = !todos.value[index].completed;
+		};
+		
+		const deleteTodo = (index) => {
+			todos.value.splice(index, 1);
+		};
 		
 		return {
-			todo,
 			todos,
-			hasError,
-			addList,
+			addTodo,
+			toggleTodo,
 			todoStyle,
 			deleteTodo,
 		};
